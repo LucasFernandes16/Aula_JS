612f53233a049fc035d1cdf9cd4fe0fad7b2443a
Lucas <lfbm@etepd.com>
2021-05-12 10:01:10 -0300

    Códigos

---
+++ main.js
@@ -0,0 +1,64 @@
+function clicou(){
+    document.getElementById("olá marilene").innerHTML = "<b>Harigato</b>" 
+    //console.log(document.getElementById("olá marilene")) 
+} 
+function redirecionar(){
+    window.open("https://www.youtube.com/watch?v=zt7nsebvS64"); 
+   //window.location.href = "https://www.youtube.com/watch?v=zt7nsebvS64" 
+} 
+function trocar(elemento){ 
+   //document.getElementById("mousemove"). innerHTML = "Obrigado por passar el mausito" 
+   //elemento.innerHTML = "Obrigado " 
+   //}
+   //    alert("Trocar texto") } function voltar(elemento){ //document.getElementById("mousemove"). innerHTML = "Passa o mouse aqui" elemento.innerHTML = "Passe o mouse" } 
+function load(){
+    alert("Pagina carregada") 
+}
+function funcChange(elemento) {
+    console.log(elemento.value) 
+} 
+   //var nome = "Lukinhas"; //var idade = "29"; 
+   //var idade2 = "10"; 
+   //alert("Bem vindo " + nome + "De " + idade + " Aninhos"); 
+   //alert(idade+idade2); 
+   //console.log(nome); 
+   //console.log(idade+idade2) //console.log(frase.replace("Japão", "Brasil")) 
+   //console.log(frase.uppercase()) 
+   //var lista = ["maça", "pêra", "laranja"]; 
+   //lista.pop(); 
+   //console.log(lista.length) 
+   //*var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"pêra", cor:"verde"}] 
+   //console.log(frutas); //alert(frutas[1].nome);
+   //var idade = prompt("Qual sua idade?"); 
+   //var idade = 18; 
+   //if (idade > 18){ 
+   //    alert("maior de idade"); 
+   //}else{ 
+   //    alert("menor de idade"); 
+   //}; 
+   //var count = 0; //while (count <= 5){ 
+   //    console.log(count); 
+   //   count++; 
+   //} 
+   //var count ; 
+   //for(count=0; count<=5; count++){ 
+   //    alert(count);
+    //} 
+   //var d = new Date(); 
+   //alert(d.getMonth()+1); //function soma(n1, n2){ 
+   //    return n1= n2; 
+   //} 
+   // 
+   //var validar; 
+   //function valida_idade(idade){ 
+   //    if(idade >= 18){ 
+   //        validar = true
+    //    }else{ 
+   //        validar = false 
+   //    } 
+   //    return validar 
+   //} 
+   // 
+   //alert(soma(5, 10)); 
+   //var idade = prompt("Qual sua idade") //console.log(valida_idade(idade)); 
+   //console.log(validar) //console.log(valida_idade)
